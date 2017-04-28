a23d64756fc5472e1e733fd017869853c825a160
Ross Mackie <ross.d.mackie@gmail.com>
2017-04-28 10:43:01 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -25,8 +25,8 @@ def webhook(request):
         res = json.dumps(res, indent=4)
         print(res)
         # r = make_response(res)
-        r.headers['Content-Type'] = 'application/json'
-        return r
+        res.headers['Content-Type'] = 'application/json'
+        return res
 
     else:
         print('Hello')
