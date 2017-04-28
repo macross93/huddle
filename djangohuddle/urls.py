0af3eeb14109307b22f0e1da320efbd9b8bff0bd
Ross Mackie <ross.d.mackie@gmail.com>
2017-04-28 09:25:29 +0100

    first commit

--- djangohuddle/urls.py
+++ djangohuddle/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from djangohuddle.views import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+        r'^$', hello),
 ]
