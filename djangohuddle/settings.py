4dcfa208ca6d3e0d31aebfb12b1f88842c6b2eb2
Ross Mackie <ross.d.mackie@gmail.com>
2017-04-28 14:07:20 +0100

    new commit

--- djangohuddle/settings.py
+++ djangohuddle/settings.py
@@ -133,3 +133,6 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
