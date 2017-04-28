b77774bad2b9f3bd518eca9434b60c76ebdba147
Ross Mackie <ross.d.mackie@gmail.com>
2017-04-28 11:33:15 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -29,23 +29,45 @@ def webhook(request):
 
 @csrf_exempt
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    # Checks for greeting action
+    if req.get("result").get("action") == "volunteer.new":
+        result = req.get("result")
+        originalRequest = req.get("originalRequest")
+        data = originalRequest.get("data")
+        sender = data.get("sender")
+        fb_id = sender.get("id")
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+        speech = "It's a shock we made it this far"
 
-    print("Response:")
-    print(speech)
+        return {
+            "speech": speech,
+            "displayText": speech,
+            #"data": {},
+            # "contextOut": [],
+            "source": "apiai-onlinestore-shipping"
+        }
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
-    }
+
+    if req.get("result").get("action") = "shipping.cost":
+        result = req.get("result")
+        parameters = result.get("parameters")
+        zone = parameters.get("shipping-zone")
+
+        cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+
+        speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+
+        print("Response:")
+        print(speech)
+
+        return {
+            "speech": speech,
+            "displayText": speech,
+            #"data": {},
+            # "contextOut": [],
+            "source": "apiai-onlinestore-shipping"
+        }
+
+    else:
+        return{}
