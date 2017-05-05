1cc78d26918c278a8ccd797c132eaf2f373b9b88
Ross Mackie <ross.d.mackie@gmail.com>
2017-05-05 07:38:28 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -80,14 +80,15 @@ def makeWebhookResult(request):
 
         try:
             u = user.objects.get(facebook_id=fb_id)
+            speech = u
 
         except user.DoesNotExist:
             speech = "You dont yet exist in my database"
             u1 = user(facebook_id=fb_id)
             u1.save
 
-        else:
-            speech = "Youre in my database " + u
+#        else:
+#            speech = "Youre in my database " + u
 
         return {
             "speech": speech,
