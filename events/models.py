d5c09653ec71f19f92ffcf31d577f59b6f40ba55
Ross Mackie <ross.d.mackie@gmail.com>
2017-05-07 10:29:32 +0100

    new commit

--- events/models.py
+++ events/models.py
@@ -41,7 +41,7 @@ class event(models.Model):
     end = models.DateTimeField()
     duration = models.DurationField(blank=True)
     charity = models.ForeignKey(charity)
-    volunteer = models.ForeignKey(user, blank=True, null=True)
+    volunteer = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.name
