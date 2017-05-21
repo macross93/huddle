c55709a628f9fb1d806d898b2fa5f3fc3a687089
Ross Mackie <ross.d.mackie@gmail.com>
2017-05-21 22:40:19 +0100

    new commit

--- djangohuddle/forms.py
+++ djangohuddle/forms.py
@@ -6,4 +6,4 @@ class eventForm(forms.ModelForm):
 
     class Meta:
         model = Post
-	fields = ('name', 'details', 'address', 'city', 'postcode', 'start', 'end', 'duration', 'charity')
+        fields = ('name', 'details', 'address', 'city', 'postcode', 'start', 'end', 'duration', 'charity')
