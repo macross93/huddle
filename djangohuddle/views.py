4368d4152b906bb521127063c48de7e340289ea7
Ross Mackie <ross.d.mackie@gmail.com>
2017-05-21 13:38:08 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -82,7 +82,7 @@ def makeWebhookResult(request):
     data = originalRequest.get("data")
     sender = data.get("sender")
     fb_id = sender.get("id")
-    page = Page(PAGE_ACCESS_TOKEN)
+    page = Page("EAAElJTd6foABAPkRNGlNHy6mxt277aJN8Yy4scRl4ViKYetPmlyZCPdbD3ZCcPt0uoANv61pZCeDDbdp20X7ukn8jZA6tX655ZBUAHDuMEg6luyGpXU3VcFaxK5ZC3DDCjhRptTZCDIIlqtW9ZBUE5WMPdPZBmvwirZCsPR1vvWoQgZAQZDZD")
     user_profile = page.get_user_profile(event.sender_id) # return dict
     print(user_profile)
 
