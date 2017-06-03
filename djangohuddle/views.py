2ef84dce2be93a521390897f498be217ed316ddc
Ross Mackie <ross.d.mackie@gmail.com>
2017-06-03 11:49:33 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -307,7 +307,7 @@ def makeWebhookResult(request):
                             "default_action": {
                               "type": "web_url",
                               "url": "https://macdovey.co.uk",
-                              "messenger_extensions": true,
+#                              "messenger_extensions": true,
                               "webview_height_ratio": "tall",
                               "fallback_url": "https://macdovey.co.uk"
                             },
