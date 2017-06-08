7d568c21bd7288a967abd7bd835c39222a6e63eb
Ross Mackie <ross.d.mackie@gmail.com>
2017-06-08 08:05:17 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -422,11 +422,13 @@ def makeWebhookResult(request):
             if e.confirmed != "y":
                 e.confirmed = "y"
                 e.save()
+                print ("this was confirmed")
                 speech = "Yes! Great decision! You're in :)! Let me know if something changes and you suddenly can't make it, or feel free to keep asking for more details if you forget / want to know more."
                 contextOut = "locked_in"
             else:
                 speech = "Sorry mate! Someone must have snuck in on that opportunity when you weren't looking! Try another one..."
                 contextOut = "volunteer_timedate"
+                print ("This was not confirmed")
 
         # if request.get("result").get("action") == "details_l2_button":
         #     try:
