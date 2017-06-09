fc8d339cc431393cfe27b82bc4b3317b4b7ce6a1
Ross Mackie <ross.d.mackie@gmail.com>
2017-06-09 22:36:54 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -495,7 +495,7 @@ def makeWebhookResult(request):
         if request.get("result").get("action") == "details_location":
             try:
                 contexts = result.get("contexts")
-#                parameters_2 = contexts.get("parameters")
+                parameters_2 = contexts.get("parameters")
 #                payload = parameters_2.get("details-button")
 
                 eventlocation = parameters.get("event-location")
