ff55f3e4a5969bd632e5f79116c3673b89159950
Ross Mackie <ross.d.mackie@gmail.com>
2017-06-24 21:24:23 +0100

    new commit

--- djangohuddle/urls.py
+++ djangohuddle/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from djangohuddle.views import hello, webhook, signup, home, eventList, userList, charityList, charitycontactList
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
