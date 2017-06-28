a638153e6fe71dc7e4c92724878c287741fb830e
Ross Mackie <ross.d.mackie@gmail.com>
2017-06-28 20:41:24 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -13,7 +13,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-import forms
+from forms import DocumentForm
 
 import json
 import urllib
