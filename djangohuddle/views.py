7a9453eb11dfdb47f17af4865de8ee46cab0f748
Ross Mackie <ross.d.mackie@gmail.com>
2017-07-02 22:47:55 +0100

    new commit

--- djangohuddle/views.py
+++ djangohuddle/views.py
@@ -509,6 +509,10 @@ def makeWebhookResult(request):
                 if e.confirmed != "y":
                     e.confirmed = "y"
                     e.save()
+                    e3 = event(name=e1.name, details=e1.details, address=e1.address, city=e1.city, postcode=e1.postcode, start=e1.start, end=e1.end, charity=e1.charity, image=e1.image, volunteer="",confirmed="n")
+                    e3.save()
+                    e4 = event.objects.filter(name=e1.name, confirmed="n")[0]
+                    print (e4.details)
                     speech = "Yes! Great decision! You're in :)! Let me know if something changes and you suddenly can't make it, or feel free to keep asking for more details if you forget / want to know more."
                     contextOut = "locked_in"
 
