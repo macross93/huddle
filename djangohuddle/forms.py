ad81db55f0482260158d71d18eb31689e4d1aec4
Ross Mackie <ross.d.mackie@gmail.com>
2017-07-09 17:45:19 +0100

    new commit

--- djangohuddle/forms.py
+++ djangohuddle/forms.py
@@ -12,6 +12,10 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = event
         fields = ('name', 'details', 'address', 'city', 'postcode', 'start', 'end', 'charity', 'image')
+
+        end = forms.DateTimeField(widget=DateTimeWidget(usel10n=True, bootstrap_version=3))
+        start = forms.DateTimeField(widget=DateTimeWidget(usel10n=True, bootstrap_version=3))
+
         widgets = {
             'end': DateTimeWidget(attrs = {'id':'id_dateTimeField'}, bootstrap_version=3, usel10n=True),
             'start': DateTimeWidget(attrs = {'id':'id_dateTimeField'}, bootstrap_version=3, usel10n=True),
